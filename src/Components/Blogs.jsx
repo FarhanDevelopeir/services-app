2af7872dcea216723301818dc57fd6023190aa3e
Muhammad farhan <fb4459467@gmail.com>
2024-06-12 06:58:32 -0700

    animated blogs

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -19,18 +19,19 @@ const truncateDescription = (description) => {
   return (
     <div className='w-[90%] m-auto' >
       <div >
-        <h1 className='heading text-center ' >Recent Blogs</h1>
+        <h1 className='heading text-center ' >Our Blogs</h1>
       </div>
-      <div className=' my-20'>
+      <div className=' overflow-hidden relative'>
+      <div className='slide-track flex gap-5 my-20'>
         {blogs.blogs.map((item, index) => {
           // Use the index to cycle through the thumbnails
           const thumbnail = thumbnails[index % thumbnails.length];
           return (
-            <div key={index} className=' mb-6 border border-border_color  rounded-2xl flex justify-center p-5'>
-              <div className='w-[30%]'>
+            <div key={index} className='slide w-[10%] mb-6 border hover:border-[#d5f365] cursor-pointer border-border_color  rounded-2xl  p-5'>
+              <div className=''>
                 <img src={thumbnail} className=' w-72 h-72' alt={`Thumbnail for ${item.title}`} />
               </div>
-              <div className='w-[90%] flex'>
+              <div className='w-[90%]'>
                 <h2>{item.topic}</h2>
                 <h1>{item.title}</h1>
                 <h3>{truncateDescription(item.description)}</h3>
@@ -38,6 +39,7 @@ const truncateDescription = (description) => {
             </div>
           );
         })}
+      </div>
       </div>
     </div>
   );
