b9819a8975fca063c1d051ea1cacf46827fbd07b
Muhammad Zeeshan <79660812+ShaniDevelp@users.noreply.github.com>
2024-06-27 15:03:02 +0500

    new chnages

--- src/Components/service-pages/ServivesDetails.jsx
+++ src/Components/service-pages/ServivesDetails.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react'
+import { React, useEffect } from 'react'
 import services from "../../data/data.json";
 import { useParams } from 'react-router-dom';
 
@@ -7,26 +7,27 @@ import { useParams } from 'react-router-dom';
 
 
 const ServivesDetails = () => {
-    const { id } = useParams();
-    console.log(id);
-    // console.log(services.Services);
-    const get= services.Services
-    console.log(get);
-    const serve = get.find((item) => item.id === id);
-    console.log(serve);
+  const { id } = useParams();
+  console.log(id);
+  const get = services.Services
+  console.log(get);
+  const serve = get.find((item) => item.id === parseInt(id)); // if id should be a number
+  console.log(serve);
 
-    
-    if (!serve) {
-        return <div className=' text-white'>Service not found</div>;
-      }
-  
+
+  if (!serve) {
+    return <div className=' text-white'>Service not found</div>;
+  } else {
     return (
-        <div>
+      <div>
         {/* <div dangerouslySetInnerHTML={createMarkup(service.icon)} className="" /> */}
-        <h1 className="my-4 text-xl font-semibold sm:text-2xl ">{serve.title}</h1>
+        <h1 className="my-4 text-white text-xl font-semibold sm:text-2xl ">{serve.title}</h1>
         <p>{serve.description}</p>
       </div>
-  )
+    )
+  }
+
+ 
 }
 
 export default ServivesDetails
